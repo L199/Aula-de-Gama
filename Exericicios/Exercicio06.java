476e5057d91f9c371d8136fdd0dd9402ea106264
L199 <lucas.ferreira961245@gmail.com>
2023-02-10 12:44:40 -0300

    Aula de Gama

---
+++ Exericicios/Exercicio06.java
@@ -0,0 +1,23 @@
+package Exericicios;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner  sc= new Scanner(System.in);
+        double nota;
+
+        System.out.println("Digite a nota: ");
+        nota= sc.nextDouble();
+
+        if(nota<7.0){
+            if(nota>=5){
+                System.out.println("Exame");
+            }else{
+                System.out.println("Repvoado");
+            }
+        }else{
+            System.out.println("Aprovado");
+        }
+    }
+}
